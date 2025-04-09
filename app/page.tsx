f36005ecc11f94409d609a959166190fe4b36c2d
jmderby <jonathan.temp@paella.dev>
2025-04-09 16:07:41 -0700

    cleaned up quickstart template

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,70 @@
-import { CrossmintProviderWrapper } from "@/components/providers/crossmint-provider";
-import { SampleComponent } from "@/components/sample/sample-component";
+import Image from "next/image";
+import { HomeContent } from "@/app/home";
 
 export default function Home() {
   return (
-    <CrossmintProviderWrapper>
-      <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        <SampleComponent />
-        {/* Add your components here */}
-      </div>
-    </CrossmintProviderWrapper>
+    <div className="grid grid-rows-[0px_1fr_60px] items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <HomeContent />
+      </main>
+      <footer className="row-start-3 flex flex-col gap-4 items-center justify-center">
+        <div className="flex gap-6 flex-wrap items-center justify-center">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://github.com/crossmint/link-to-repo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            View code
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://github.com/crossmint"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/window.svg"
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            See all quickstarts
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://crossmint.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            Go to crossmint.com →
+          </a>
+        </div>
+        <div className="flex">
+          <Image
+            src="/crossmint-leaf.svg"
+            alt="Powered by Crossmint"
+            priority
+            width={152}
+            height={100}
+          />
+        </div>
+      </footer>
+    </div>
   );
 }
